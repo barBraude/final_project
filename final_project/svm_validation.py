18ed89e0c50ddf34ceb366b36ca9cce91a65c7e8
barBraude <119129652+barBraude@users.noreply.github.com>
2022-12-30 12:00:22 +0200

    initial commit

---
+++ final_project/svm_validation.py
@@ -0,0 +1,144 @@
+import joblib
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn import metrics
+import pandas as pd
+import seaborn as sn
+
+# load the model
+svm_loaded_model = joblib.load('svm_Model_24.12.pkl')
+model = svm_loaded_model.best_estimator_
+# test values for the model
+x_test_scaled = joblib.load('svm_Model_X_test_scaled.csv')
+# labels for the test values
+y_test = joblib.load('svm_Model_y_test.csv')
+label_test = pd.Series(y_test).array
+label_prediction = model.predict(x_test_scaled)
+# print(label_prediction)
+ 
+confusion_matrix = metrics.confusion_matrix(label_prediction,label_test, labels=["Low", "Meduim", "High"])
+reported = metrics.classification_report(label_prediction,label_test,labels=["Low", "Meduim", "High"])
+print(reported)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = ["Low", "Meduim", "High"])
+
+cm_display.plot()
+plt.show()
+
+
+print("length:",len(label_test))
+print("length:",len(label_prediction))
+
+print(f'Low Risk')
+
+TP = 0 #The model correctly identifies a case as Low-Risk
+FP = 0 #The model incorrectly identifies a case as Low-Risk
+FN = 0 #The model incorrectly identifies a case as Medium-Risk or High-Risk
+TN = 0 #The model correctly identifies a case as Medium-Risk or High-Risk
+
+# Low Risk
+for true,pred in zip(label_test,label_prediction):
+      
+
+
+    if true == 'Low' and pred == 'Low' :
+        TP += 1
+    elif (pred == 'Meduim' or pred == 'High') and (true == 'Meduim' or true == 'High'):
+        TN += 1
+    elif pred == 'Low' and (true == 'Meduim' or true == 'High'):
+        FP += 1
+    elif (pred == 'Meduim' or pred == 'High') and true == 'Low':
+        FN += 1
+
+
+TPR = TP / (TP + FN)
+FPR = 1 - (TN / (TN + FP))
+Accuracy = (TP + TN) / (TP + TN + FP + FN)
+Precision = TP / (TP + FP)
+
+print(f'True Positive  {TP}')
+print(f'True Negative {TN}')
+print(f'False Positive {FP}')
+print(f'False Negative {FN}')
+
+# print(f'True Positive Rate (TPR) {TPR}')
+# print(f'False Positive Rate (FPR) {FPR}')
+print(f'Accuracy  {Accuracy}')
+print(f'Precision  {Precision}')  
+
+print(f'Medium Risk')
+
+TP = 0 #The model correctly identifies a case as Low-Risk
+FP = 0 #The model incorrectly identifies a case as Low-Risk
+FN = 0 #The model incorrectly identifies a case as Medium-Risk or High-Risk
+TN = 0 #The model correctly identifies a case as Medium-Risk or High-Risk
+
+
+# Medium Risk
+for true,pred in zip(label_test,label_prediction):
+      
+
+    # 2 - positive, 3 - negative
+    if true == 'Meduim' and pred == 'Meduim' :
+        TP += 1
+    elif (pred == 'Low' or pred == 'High') and (true == 'Low' or true == 'High'):
+        TN += 1
+    elif pred == 'Meduim' and (true == 'Low' or true == 'High'):
+        FP += 1
+    elif (pred == 'Low' or pred == 'High') and true == 'Meduim':
+        FN += 1
+
+
+TPR = TP / (TP + FN)
+FPR = 1 - (TN / (TN + FP))
+Accuracy = (TP + TN) / (TP + TN + FP + FN)
+Precision = TP / (TP + FP)
+
+print(f'True Positive  {TP}')
+print(f'True Negative {TN}')
+print(f'False Positive {FP}')
+print(f'False Negative {FN}')
+
+# print(f'True Positive Rate (TPR) {TPR}')
+# print(f'False Positive Rate (FPR) {FPR}')
+print(f'Accuracy  {Accuracy}')
+print(f'Precision  {Precision}')  
+
+
+print(f'High Risk')
+
+TP = 0 #The model correctly identifies a case as Low-Risk
+FP = 0 #The model incorrectly identifies a case as Low-Risk
+FN = 0 #The model incorrectly identifies a case as Medium-Risk or High-Risk
+TN = 0 #The model correctly identifies a case as Medium-Risk or High-Risk
+
+
+# High Risk
+for true,pred in zip(label_test,label_prediction):
+      
+
+    # 2 - positive, 3 - negative
+    if true == 'High' and pred == 'High' :
+        TP += 1
+    elif (pred == 'Low' or pred == 'Meduim') and (true == 'Low' or true == 'Meduim'):
+        TN += 1
+    elif pred == 'High' and (true == 'Low' or true == 'Meduim'):
+        FP += 1
+    elif (pred == 'Low' or pred == 'Meduim') and true == 'High':
+        FN += 1
+
+
+TPR = TP / (TP + FN)
+FPR = 1 - (TN / (TN + FP))
+Accuracy = (TP + TN) / (TP + TN + FP + FN)
+Precision = TP / (TP + FP)
+
+print(f'True Positive  {TP}')
+print(f'True Negative {TN}')
+print(f'False Positive {FP}')
+print(f'False Negative {FN}')
+
+# print(f'True Positive Rate (TPR) {TPR}')
+# print(f'False Positive Rate (FPR) {FPR}')
+print(f'Accuracy  {Accuracy}')
+print(f'Precision  {Precision}')  
