e48309957a73a1d6fe06c412ccd84e523416c67b
rinatst <rinat20968@gmail.com>
2023-01-13 18:55:02 +0200

    final changes in UI

--- gui/main.py
+++ gui/main.py
@@ -16,18 +16,19 @@ class MyWindow(QWidget):
         self.opening_page = QWidget()
         #create title label
         self.title_label = QLabel(self)
-        self.title_label.setGeometry(10,20,300,100)
+        self.title_label.setGeometry(45,120,400,100)
         self.text_edit=QTextEdit()
-        font = QFont("Segoe UI ", 7)
+        font = QFont("Segoe UI ", 10)
         # Set the font of the QLabel widget
         self.title_label.setFont(font)
         # Set the text of the QLabel widget
         self.title_label.setText("<h1>Welcome to CVD risk prediction App<h1>")
         self.title_label.setWordWrap(True)
+        self.title_label.setStyleSheet("color: #E3DECB;")
         # Create a QPalette object
         palette = QPalette()
         # Load the image and create a QBrush object with it
-        image = QImage(QPixmap("back-pain-2.jpg"))
+        image = QImage(QPixmap("heart-attack.png"))
         #brush = QBrush()
         pixmap = QPixmap(image)
        
@@ -44,11 +45,11 @@ class MyWindow(QWidget):
         # Apply the palette to the application
         self.opening_page.setPalette(palette)
         self.opening_page.setAutoFillBackground(True)
-        
+        self.opening_page.setStyleSheet("position : 100px ;")
         self.opening_button = QPushButton('Go to prediction', self.opening_page)
         # Set the button location and size
         self.opening_button.setGeometry(160, 400, 150, 40)
-        self.opening_button.setStyleSheet("font: 9pt Segoe UI Semibold; background-color: #F0F0E4")
+        self.opening_button.setStyleSheet("font: 9pt Segoe UI Semibold; background-color: #EEEEEA")
         self.opening_button.clicked.connect(self.go_to_main_page)
         # Create the main page
         self.main_page = QWidget()
@@ -56,31 +57,34 @@ class MyWindow(QWidget):
         # Create a button to open the file dialog
         self.open_csv_button = QPushButton('Open CSV', self)
 
-        self.open_csv_button.setStyleSheet("font: 9pt Segoe UI Semibold; background-color: #F0F0E4")
+        self.open_csv_button.setStyleSheet("font: 9pt Segoe UI Semibold; background-color: #EEEEEA")
         self.open_csv_button.clicked.connect(self.open_csv)
         
         self.back_to_open_screen= QPushButton('Back to main screen', self)
-        self.back_to_open_screen.setStyleSheet("font: 9pt Segoe UI Semibold; background-color: #F0F0E4")
+        self.back_to_open_screen.setStyleSheet("font: 9pt Segoe UI Semibold; background-color: #EEEEEA")
         self.back_to_open_screen.clicked.connect(self.go_back_to_open_screen)
        
         #add label for descriptin
         self.desc_label = QLabel(self)
-        self.desc_label.setText("""Select data sheet of format type 'CSV' to load your data. After loading the data, a risk category will be presented based on your data""")
-        self.desc_label.setStyleSheet("font: 11pt Segoe UI Semibold; background-color: #F0F0E4")
+        self.desc_label.setText("""Select data sheet of format type 'CSV' to load your data. \nAfter loading the data, a risk category will be presented based on your data""")
+        self.desc_label.setStyleSheet("font: 11pt Segoe UI Semibold;")
         self.desc_label.setWordWrap(True)
+        
+        #result_label
+        self.res_label = QLabel(self)
+        # self.res_label.setStyleSheet("font: 14pt Segoe UI bold; background-color: #E3DECB")
+        
         # Create a table view and a model
-        self.table_view = QTableView()
+        #self.table_view = QTableView()
         self.model = QStandardItemModel(self)
-        # Set the model for the table view
-        self.table_view.setModel(self.model)
-
+    
         # Create a layout and add the table view and button
         layout = QVBoxLayout(self.main_page)
-        layout.addWidget(self.table_view)
+        # layout.addWidget(self.table_view)
         layout.addWidget(self.desc_label)
         layout.addWidget(self.open_csv_button)
         layout.addWidget(self.back_to_open_screen)
-        
+        layout.addWidget(self.res_label)
         # Add the pages to the stacked widget
         self.stacked_widget.addWidget(self.opening_page)
         self.stacked_widget.addWidget(self.main_page)
@@ -88,15 +92,14 @@ class MyWindow(QWidget):
         # Set the opening page as the current page
         self.stacked_widget.setCurrentIndex(0)
 
-         # Create a layout and add the stacked widget
+        # Create a layout and add the stacked widget
         layout = QVBoxLayout(self)
         layout.addWidget(self.stacked_widget)
 
         # Set the application icon
-        app.setWindowIcon(QIcon("sitting-icon.png"))
+        app.setWindowIcon(QIcon("heart.png")) 
 
-        
-
+    
     def open_csv(self):
         # Open the file dialog and get the selected file
         self.desc_label.hide()
@@ -119,21 +122,14 @@ class MyWindow(QWidget):
                 data = [data]
             pred = model.predict(data)
 
-
+            pred_result = ', '.join(pred)
+            self.res_label.setStyleSheet("QLabel { text-align: center; }")
+            self.res_label.setText(" Your predicted risk category is: \n " + pred_result)
+            self.setResultBackgroundColor(self.res_label,pred_result)
+            # print(pred_result)
             # Clear the model
             self.model.clear()
 
-            # Set the column names
-            self.model.setHorizontalHeaderLabels(['The Result'] )
-
-            # Add the data to the model
-            for row in pred:
-                items = [QStandardItem(str(row))]
-                self.model.appendRow(items)
-
-            # Resize the columns
-            #self.table_view.resizeColumnsToContents()
-            # print(pred)
 
     def go_to_main_page(self):
         self.stacked_widget.setCurrentIndex(1)
@@ -143,6 +139,17 @@ class MyWindow(QWidget):
     def go_back_to_open_screen(self):
          self.stacked_widget.setCurrentIndex(0)
          self.title_label.show()
+
+    def setResultBackgroundColor(self,window,color):
+        color_dict = {
+            "Low": "#9ED6A5",
+            "Meduim": "#E8EDA9",
+            "High": "#FF7575"
+        }
+        try:
+            window.setStyleSheet("background-color: {}".format(color_dict[color]))
+        except KeyError:
+            print("Invalid color")
 
 if __name__ == '__main__':
     app = QApplication([])
